9be5094f3273a0a15454841c88b02300db9494aa
Dinko <dinko.dugec@gmail.com>
2022-01-14 10:32:44 +0100

    show doctor all appointments

--- app/Appointment.php
+++ app/Appointment.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Appointment extends Model
 {
    protected $guarded = [];
+
+   public function doctor()
+   {
+ 
+      return $this->BelongsTo(User::class,'user_id','id');
+
+   }
 }
