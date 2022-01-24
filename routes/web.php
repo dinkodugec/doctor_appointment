17aaf770f5e18c8271595ed6e0cc75b2bcafe6c5
Dinko <dinko.dugec@gmail.com>
2022-01-24 22:11:41 +0100

    making prescription table and migrate

--- routes/web.php
+++ routes/web.php
@@ -50,10 +50,8 @@ Route::get('/home', 'HomeController@index')->name('home');
       Route::get('status/update/{id}', 'PatientlistController@toggleStatus')->name('update.status');
     });
 
-Route::group(['middleware'=>['auth','doctor']], function(){
-
-Route::resource('appointment', 'AppointmentController');
-Route::post('/appointment/check', 'AppointmentController@check')->name('appointment.check');
-
-Route::post('/appointment/update', 'AppointmentController@updateTime')->name('update');
-});
+   Route::group(['middleware'=>['auth','doctor']], function(){
+      Route::resource('appointment', 'AppointmentController');
+      Route::post('/appointment/check', 'AppointmentController@check')->name('appointment.check');
+      Route::post('/appointment/update', 'AppointmentController@updateTime')->name('update');
+    });
